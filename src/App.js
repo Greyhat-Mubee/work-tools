b695b70f588c79e1b472f7637ccc804e8b91b01f
dy9amix <segunorodeji@gmail.com>
2020-12-29 15:53:28 +0100

    Fixed emergency issue for build

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ const styles = StyleSheet.create({
 
 class App extends Component{
   state = { selectedItem: 'Login',
-            isSignedIn: true,
+            isSignedIn: false,
             auth_token: "",
             name: ""
     };
